a09499086f995d70dd8ccd92f1b52a72ceb81eb3
whitesparrow17 <whitesparrowrose@gmail.com>
2016-01-12 21:26:22 -0500

    Start of Robot

---
+++ src/Robot.cpp
@@ -0,0 +1,60 @@
+#include "WPILib.h"
+#include "constants.h"
+
+class Robot: public SampleRobot
+{
+   Talon m_flywheels;
+   Joystick m_joystick;
+   Joystick m_gamepad;
+
+   RobotDrive driveTrain;
+
+public:
+   Robot() :
+      m_flywheels(Port::flywheels),
+      m_joystick(Port::joystick),
+      m_gamepad(Port::gamepad),
+
+      driveTrain(Port::frontLeftWheelMotor,
+            Port::rearLeftWheelMotor,
+            Port::frontRightWheelMotor,
+            Port::rearRightWheelMotor)
+   {
+
+   }
+
+   void OperatorControl()
+   {
+      while(IsOperatorControl() && IsEnabled())
+      {
+         float throttle = - m_joystick.GetY();
+         if (abs(throttle) < 0.05f) //This makes a deadzone
+         {
+             throttle = 0;
+         }
+         float twist = m_joystick.GetZ();
+         if (abs(twist) < 0.05f) //This also makes a deadzone
+         {
+            twist = 0;
+         }
+         float throttleRatio = 0.7f;// .8 is too high :(
+         float twistRatio = 1 - throttleRatio;
+         float leftPower = (throttle * throttleRatio) + (twist * twistRatio);
+         float rightPower = (throttle * throttleRatio) - (twist * twistRatio);
+
+         driveTrain.TankDrive(leftPower, rightPower);
+
+         if(m_gamepad.GetRawButton(1))
+         {
+            m_flywheels.Set(1);
+         }
+         if(m_gamepad.GetRawButton(2))
+         {
+            m_flywheels.Set(0);
+         }
+      }
+   }
+
+};
+
+START_ROBOT_CLASS(Robot);
---
+++ src/constants.h
@@ -0,0 +1,24 @@
+#ifndef SRC_CONSTANTS_H_
+#define SRC_CONSTANTS_H_
+#include "WPILib.h"
+
+struct Port{
+// Channels for wheels
+   const static uint32_t frontLeftWheelMotor   = 1;
+   const static uint32_t rearLeftWheelMotor    = 0;
+   const static uint32_t frontRightWheelMotor  = 2;
+   const static uint32_t rearRightWheelMotor   = 3;
+
+//Flywheels
+   const static uint32_t flywheels = 5; //Both on the same motor controller
+
+// Controllers
+   const static uint32_t joystick = 0;
+   const static uint32_t gamepad = 1;
+
+
+
+};
+
+
+#endif /* SRC_CONSTANTS_H_ */
