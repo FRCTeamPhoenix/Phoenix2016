ff9c88e113b50a9b000bbb73dfe7a12fa266cfee
whitesparrow17 <whitesparrowrose@gmail.com>
2016-01-14 20:05:22 -0500

    Added AutoController

---
+++ src/AutoController.cpp
@@ -0,0 +1,18 @@
+/*
+ * AutoController.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: Rose
+ */
+
+#include <AutoController.h>
+
+AutoController::AutoController() {
+   // TODO Auto-generated constructor stub
+
+}
+
+AutoController::~AutoController() {
+   // TODO Auto-generated destructor stub
+}
+
---
+++ src/AutoController.h
@@ -0,0 +1,17 @@
+/*
+ * AutoController.h
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: Rose
+ */
+
+#ifndef SRC_AUTOCONTROLLER_H_
+#define SRC_AUTOCONTROLLER_H_
+
+class AutoController {
+public:
+   AutoController();
+   virtual ~AutoController();
+};
+
+#endif /* SRC_AUTOCONTROLLER_H_ */
