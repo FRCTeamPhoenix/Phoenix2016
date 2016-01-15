4d257d85d4c3582a3d0566253e30e49585d8fda8
IanCoolidge0 <ian.coolidge@asdnh.org>
2016-01-14 19:58:08 -0500

    create manual controller class

---
+++ src/ManualController.cpp
@@ -0,0 +1,18 @@
+/*
+ * ManualController.cpp
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: Ian
+ */
+
+#include <ManualController.h>
+
+ManualController::ManualController() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ManualController::~ManualController() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/ManualController.h
@@ -0,0 +1,17 @@
+/*
+ * ManualController.h
+ *
+ *  Created on: Jan 14, 2016
+ *      Author: Ian
+ */
+
+#ifndef SRC_MANUALCONTROLLER_H_
+#define SRC_MANUALCONTROLLER_H_
+
+class ManualController {
+public:
+	ManualController();
+	virtual ~ManualController();
+};
+
+#endif /* SRC_MANUALCONTROLLER_H_ */
