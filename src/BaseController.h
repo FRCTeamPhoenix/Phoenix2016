8d376517e2881522354cda1b61b84f280666b9a0
willgebhardt <William.Gebhardt@asdnh.org>
2016-01-15 20:33:00 -0500

    Added The base class to index

---
+++ src/BaseController.h
@@ -0,0 +1,16 @@
+/*
+ * BaseController.h
+ *
+ *  Created on: Jan 15, 2016
+ *      Author: Will
+ */
+
+#ifndef SRC_BASECONTROLLER_H_
+#define SRC_BASECONTROLLER_H_
+
+class BaseController {
+public:
+   virtual void run() = 0;
+};
+
+#endif /* SRC_BASECONTROLLER_H_ */
