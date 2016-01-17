38dcd77b180fc196ff219bd4e000464b257a860a
Ben Nichols-Farquhar <erogone775@gmail.com>
2016-01-17 14:52:24 -0500

    Code compiles and is ready to test for the client

--- src/Client.cpp
+++ src/Client.cpp
@@ -1,14 +1,11 @@
 #include "Client.h"
-#include <arpa/inet.h> // winsock2.h for windows
-#include <netinet/in.h>
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <unistd.h>
 #include <string.h>
+#include <iostream>
+using namespace std;
 #define NPACK 1
 #define PORT 31415
-#define SRV_IP "999.999.999.999" //change ip address
+#define SRV_IP "10.0.42.21" //change ip address
 
 
 
@@ -17,37 +14,40 @@ Client::Client() {
 //   std::thread receiveThread(receivePacket);
 
 }
+void Client::initilizeSocket(){
+   m_socket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
+   memset((char *) &m_si_me, 0, sizeof(m_si_me));
+
+   m_si_me.sin_family = AF_INET;
+   m_si_me.sin_port = htons(PORT);
+   m_si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+   m_si_other.sin_family = AF_INET;
+   m_si_other.sin_port = htons(PORT);
+   m_si_other.sin_addr.s_addr = inet_addr(SRV_IP);
+
+   memset(m_receivedData,0,BUFLEN);
+
+
+   sendto(m_socket,"start",5, 0 ,(sockaddr*)&m_si_other, sizeof(m_si_other));
+
+
+    }
 void Client::receivePacket(){
-
-    struct sockaddr_in si_me, si_other;
-        int receiveSocket, slen=sizeof(si_other);
-        //char buf[BUFLEN];
-
-        receiveSocket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-        memset((char *) &si_me, 0, sizeof(si_me));
-
-        si_me.sin_family = AF_INET;
-
-        si_me.sin_port = htons(PORT);
-
-        si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-
-        bind(receiveSocket, (sockaddr*)&si_me, sizeof(si_me));
-
-        while (true){
+            socklen_t m_si_other_len=sizeof(m_si_other);
+            while (true){
 
             // breaks out of loop in case of error
-            if(recvfrom(receiveSocket,m_receivedData,BUFLEN,0, (sockaddr*)&si_other, (socklen_t*)&slen) == -1) {
+            if(recvfrom(m_socket,m_receivedData,BUFLEN, 0 ,(sockaddr*)&m_si_other, &m_si_other_len) == -1) {
                break;
             }
-            //m_receivedData=buf;
+            cout << "received packet";
         }
 }
 
 void Client::sendPacket() {
-
+    sendto(m_socket,m_sendData,BUFLEN, 0 ,(sockaddr*)&m_si_other, sizeof(m_si_other));
 }
 
 Client::~Client() {
--- src/Client.h
+++ src/Client.h
@@ -1,15 +1,25 @@
 #ifndef SRC_CLIENT_H_
 #define SRC_CLIENT_H_
 
+#include <arpa/inet.h> // winsock2.h for windows
+#include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 #define BUFLEN 1024
 
 class Client {
 public:
-    static double m_targetPosition;
     char m_receivedData[BUFLEN];
     static bool receive;
+    int m_socket;
+    char m_sendData[BUFLEN];
+    sockaddr_in m_si_me, m_si_other;
 
     Client();
+
+    void initilizeSocket();
 
     void receivePacket();
 
