788b03d0cdf5840965ba43403e0edaffa4fc0e03
Ben Nichols-Farquhar <erogone775@gmail.com>
2016-01-18 22:57:09 -0500

    Fixed a small bug where the wrong line got copied, code is ready to test

--- src/Client.cpp
+++ src/Client.cpp
@@ -18,14 +18,15 @@ void Client::initilizeSocket(){
    m_socket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
    int sendCount=0;
    m_initGood=false;
+   //defining and setting socket timeout
    struct timeval timeout;
    timeout.tv_sec=0;
    timeout.tv_usec=100;
-   m_socket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+   setsockopt(m_socket, SOL_SOCKET, SO_RCVTIMEO,&timeout,sizeof(timeout));
 
-
-
+   //Initializing everything to zero
    memset((char *) &m_si_me, 0, sizeof(m_si_me));
+
 
    m_si_me.sin_family = AF_INET;
    m_si_me.sin_port = htons(PORT);
@@ -48,12 +49,10 @@ void Client::initilizeSocket(){
 void Client::receivePacket(){
         socklen_t m_si_other_len=sizeof(m_si_other);
         while (true){
-
         // breaks out of loop in case of error
         if(recvfrom(m_socket,m_receivedData,BUFLEN, 0 ,(sockaddr*)&m_si_other, &m_si_other_len) == -1) {
            break;
         }
-        cout << "received packet";
      }
 }
 
