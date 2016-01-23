e1e59e843d05e3eb5c0b470965cdc8160ef98edf
Brin Harper <brin.harper@team2342.org>
2016-01-22 19:11:02 -0500

    Added aiming class files

---
+++ src/Aiming.h
@@ -0,0 +1,17 @@
+/*
+ * Aiming.h
+ *
+ *  Created on: Jan 22, 2016
+ *      Author: Brin Harper
+ */
+
+#ifndef SRC_AIMING_H_
+#define SRC_AIMING_H_
+
+class Aiming {
+public:
+   Aiming();
+   virtual ~Aiming();
+};
+
+#endif /* SRC_AIMING_H_ */
