613e30cf88d71600303467b83de0d118eb3df153
Brin Harper <brin.harper@team2342.org>
2016-01-22 21:12:05 -0500

    Started aiming classes, working on interpreting coordinate image data

--- src/Aiming.cpp
+++ src/Aiming.cpp
@@ -3,16 +3,52 @@
  *
  *  Created on: Jan 22, 2016
  *      Author: Brin Harper
+ *
+ * The Aiming class will take in the vision data received from the Raspberry Pi by way
+ * of the UDP client, and work to implement in order to aim the shooter at the high
+ * targets.  Currently, the specific data to be received by the Raspberry Pi is unknown,
+ * so the class is configured to take in a character array representing the image data.
+ * Optimally, the client will later be set up to only receive highly mandatory data from
+ * the Raspberry Pi, giving a coordinate representation of the target's spatial location.
+ *
  */
 
 #include <Aiming.h>
 
-Aiming::Aiming() {
-   // TODO Auto-generated constructor stub
+Aiming::Aiming(Client* client) :
+   m_client(client)
+{
+
+}
+
+// Gives Aiming class access to image data sent over to client from Raspberry Pi
+int* Aiming::getNewImageData() {
+
+   //Assuming byte array represents a String
+   //TODO: change Client BUFLEN to 248
+   char originalBits[] = m_client->getData();
+
+
+
+   // This loop will convert each byte to a character (looping through 8 bits at a time;
+   // there are 8 bits in a byte)
+   for (int i = 0; i < ARRAY_SIZE(originalBits) - 8; i += 8) {
+
+   }
+   delete originalBits;
+   return *m_coordinateIntData;
+}
+
+// Centers the robot about the target (based on image-detected coordinates)
+void Aiming::align() {
+
+}
+
+// This will be called in Robot.cpp to implement all aiming mechanisms
+void Aiming::run() {
 
 }
 
 Aiming::~Aiming() {
-   // TODO Auto-generated destructor stub
 }
 
--- src/Aiming.h
+++ src/Aiming.h
@@ -3,15 +3,33 @@
  *
  *  Created on: Jan 22, 2016
  *      Author: Brin Harper
+ *
+ * The Aiming class will take in the vision data received from the Raspberry Pi by way
+ * of the UDP client, and work to implement in order to aim the shooter at the high
+ * targets.  Currently, the specific data to be received by the Raspberry Pi is unknown,
+ * so the class is configured to take in a character array representing the image data.
+ * Optimally, the client will later be set up to only receive highly mandatory data from
+ * the Raspberry Pi, giving a coordinate representation of the target's spatial location.
+ *
  */
 
 #ifndef SRC_AIMING_H_
 #define SRC_AIMING_H_
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
 class Aiming {
 public:
-   Aiming();
+   Aiming(Client*);
+   int* getNewImageData();
+   void align();
+   void run();
    virtual ~Aiming();
+
+private:
+   Client* m_client;
+   char m_coordinateCharData[31];
+   int m_coordinateIntData[8];
+
 };
 
 #endif /* SRC_AIMING_H_ */
