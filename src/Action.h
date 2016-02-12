ec89e5db43426184d9209e7e479df8411f970570
fvla <frederick.v.l@gmail.com>
2016-02-11 19:01:00 -0500

    Turned Action class into an abstract base class.

--- src/Action.h
+++ src/Action.h
@@ -16,18 +16,9 @@ enum ActionType { NO_ACTION,
 class Action
 {
  public:
-   Action(DriveStation*, DriveTrainController*, ActionType, int, float*);
-   bool execute(void);
-   ActionType getAction(void);
- private:
-   bool m_invalid;
-   bool m_firstTime;
-   DriveStation* m_controllers;
-   DriveTrainController* m_driveTrain;
-   ActionType m_action;
-   int m_argc;
-   float* m_argv;
-   Timer* m_timer;
-
-   bool waitUntil(int buttonCode);
+   Action(void);
+   void init(void) = 0;
+   bool execute(void) = 0;
+ protected:
+   bool m_initialized;
 };
