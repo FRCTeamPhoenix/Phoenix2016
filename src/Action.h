21a0484ccf90987f7aa3462f8086cbfbb27c2b39
fvla <frederick.v.l@gmail.com>
2016-02-11 20:07:22 -0500

    Removed extraneous lines from Action.h.

--- src/Action.h
+++ src/Action.h
@@ -1,18 +1,5 @@
 #pragma once
 
-class Timer;
-
-class DriveStation;
-class DriveTrainController;
-
-enum ActionType { NO_ACTION,
-		  ACTION_A,
-		  ACTION_B,
-		  ACTION_X,
-		  ACTION_DRIVE,
-		  ACTION_TURN,
-		  ACTION_BRAKE, };
-
 class Action
 {
  public:
