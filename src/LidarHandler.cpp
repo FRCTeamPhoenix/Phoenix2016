ec795afddbf775fd5b9959a354228356b5151460
willgebhardt <William.Gebhardt@asdnh.org>
2016-02-21 15:11:47 -0500

    Added Lidar

---
+++ src/LidarHandler.cpp
@@ -0,0 +1,75 @@
+/*
+ * LidarHandler.cpp
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: Ian
+ */
+
+#include <LidarHandler.h>
+
+LidarHandler::LidarHandler(Relay * onSwitch, double offset, uint32_t lidarPort):
+   m_onSwitch(onSwitch),
+   m_counter(lidarPort),
+   m_offset(offset) {
+   m_onSwitch->Set(Relay::kOn);
+   m_counter.SetSemiPeriodMode(true);
+   m_resetCount = 0;
+   m_storedCounter = 0;
+
+   m_fastAverage = 0;
+   m_mediumAverage = 0;
+   m_slowAverage = 0;
+
+   for(int i=0;i<LidarConstants::numberStoredValues;i++)
+      m_storedDistances[i] = 0;
+}
+
+double LidarHandler::getFastAverage() {
+   return m_fastAverage;
+}
+
+double LidarHandler::getMediumAverage() {
+   return m_mediumAverage;
+}
+
+double LidarHandler::getSlowAverage() {
+   return m_slowAverage;
+}
+
+void LidarHandler::run() {
+    double distance = m_counter.GetPeriod() * 100000;
+
+    //this will be true if distance is inf or NaN (i.e. the lidar has crashed)
+    bool infDistance = distance - distance != 0;
+
+    if(infDistance) {
+       SmartDashboard::PutBoolean("DB/Button 1",false);
+       m_resetCount++;
+       m_onSwitch->Set(Relay::kOff);
+       SmartDashboard::PutString("DB/String 1", "resetting");
+       Wait(0.3);
+       SmartDashboard::PutString("DB/String 1", " ");
+       m_onSwitch->Set(Relay::kOn);
+    } else {
+       m_distance = distance - m_offset;
+
+       m_storedDistances[m_storedCounter % LidarConstants::numberStoredValues] = distance;
+       m_storedCounter++;
+
+       m_fastAverage = (4 * m_fastAverage + m_distance) / 5.0;
+       m_mediumAverage = (10 * m_mediumAverage + m_distance) / 11.0;
+       m_slowAverage = (25 * m_slowAverage + m_distance) / 26.0;
+    }
+}
+
+double LidarHandler::getDistance() {
+   return m_distance;
+}
+
+int LidarHandler::getResetCount() {
+   return m_resetCount;
+}
+
+LidarHandler::~LidarHandler() {
+
+}
---
+++ src/LidarHandler.h
@@ -0,0 +1,78 @@
+/*
+ * LidarHandler.h
+ *
+ *  Created on: Feb 2, 2016
+ *      Author: Ian
+ */
+
+#ifndef SRC_LIDARHANDLER_H_
+#define SRC_LIDARHANDLER_H_
+
+#include "WPILib.h"
+
+#include "constants.h"
+
+#define HIGH 1
+#define LOW 0
+
+class LidarHandler {
+
+   //Relay controlling power cycles
+   Relay * m_onSwitch;
+
+   //Counter that gets lidar period
+   Counter m_counter;
+
+   //current lidar distance
+   double m_distance;
+
+   //slow, medium, fast averages
+   double m_slowAverage;
+   double m_mediumAverage;
+   double m_fastAverage;
+
+   //previous stored distances
+   double m_storedDistances[LidarConstants::numberStoredValues];
+
+   //robot specific offset
+   double m_offset;
+
+   //number of resets since start
+   int m_resetCount;
+
+   //counter for distance number
+   int m_storedCounter;
+
+public:
+   /**
+    * Constructor. Takes in onSwitch analog relay, offset double, and the port number for the lidar.
+    */
+   LidarHandler(Relay * m_onSwitch, double offset, uint32_t lidarPort);
+
+   /**
+    * Updates the public current distance based on lidar input.
+    * Also power-cycles it if needed.
+    */
+   void run();
+
+   /**
+    * Return the current distance from the lidar
+    */
+   double getDistance();
+
+   /**
+    * Return the lidar reset count
+    */
+   int getResetCount();
+
+   /**
+    * Return fast, medium, and slow averages
+    */
+   double getFastAverage();
+   double getMediumAverage();
+   double getSlowAverage();
+
+   virtual ~LidarHandler();
+};
+
+#endif /* SRC_LIDARHANDLER_H_ */
